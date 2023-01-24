a1de6f79cd3aae6c7efd1e3440abf47f6da097dd
onusra <onusra@angelo.edu>
2023-01-24 21:32:52 +0000

    creating an obj class

--- src/main/java/InstantiationExample.java
+++ src/main/java/InstantiationExample.java
@@ -18,6 +18,7 @@ public class InstantiationExample {
      * @return any object.
      */
     public Object instantiateANewObject(){
-        return null;
+        InstantiationExample obj = new InstantiationExample();
+        return obj;
     }
 }
